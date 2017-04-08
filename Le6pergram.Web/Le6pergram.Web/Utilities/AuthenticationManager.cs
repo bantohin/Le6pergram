0cf13a7602724b57a75e39d2b5a549094464a2f4
DanailIliew <danaililiew@gmail.com>
2017-04-08 21:48:06 +0300

    TUkastivii

--- Le6pergram.Web/Le6pergram.Web/Utilities/AuthenticationManager.cs
+++ Le6pergram.Web/Le6pergram.Web/Utilities/AuthenticationManager.cs
@@ -7,5 +7,46 @@ namespace Le6pergram.Web.Utilities
 {
     public class AuthenticationManager
     {
+        static User currentUser;
+
+        static bool IsUserExisting(string username)
+        {
+            using (Le6pergramDatabase context = new Le6pergramDatabase())
+            {
+                return context.Users.Any(u => u.Name == username);
+            }
+        }
+
+        static void SetCurrentUser(string username)
+        {
+            if (IsUserExisting(username))
+            {
+                currentUser = GetLoggedUser(username);
+            }
+            else
+            {
+                throw new InvalidOperationException("u suk");
+            }
+        }
+
+        static User GetAuthenticated()
+        {
+            if(currentUser != null)
+            {
+                return currentUser;
+            }
+            else
+            {
+                throw new InvalidOperationException("u suk");
+            }
+        }
+
+        static User GetLoggedUser(string username)
+        {
+            using (Le6pergramDatabase context = new Le6pergramDatabase())
+            {
+                return context.Users.Where(u => u.Username == username).FirstOrDefault();
+            }
+        }
     }
 }
