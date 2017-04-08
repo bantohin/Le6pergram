1df1a18e1065fe0ded07bd96db4a4c7b1bfd7f14
dundurlunka <dundurlunka@abv.bg>
2017-04-08 20:37:20 +0300

    Daniel 1.1

--- Le6pergram.Web/Le6pergram.Web/Configurations/UserConfiguration.cs
+++ Le6pergram.Web/Le6pergram.Web/Configurations/UserConfiguration.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Web;
 
 namespace Le6pergram.Web.Configurations
 {
-    public class UserConfiguration
+    public class UserConfiguration : EntityTypeConfiguration<User>
     {
+        public UserConfiguration()
+        {
+            this.HasMany(u => u.Followers).WithMany(f => f.Following).Map(uf =>
+            {
+                uf.MapLeftKey("UserId");
+                uf.MapRightKey("FollowerId");
+                uf.ToTable("UsersFollowers");
+            });
+            this.Ignore(u => u.RepeatPassword);
+        }
     }
 }
