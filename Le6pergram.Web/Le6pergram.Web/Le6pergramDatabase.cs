7506f0a2ea6bad61cf3e0edf1473f52dea5e3d10
DanailIliew <danaililiew@gmail.com>
2017-04-08 20:08:25 +0300

    Viu

---
+++ Le6pergram.Web/Le6pergram.Web/Le6pergramDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+public class Le6pergramDatabase : DbContext
+{
+    // You can add custom code to this file. Changes will not be overwritten.
+    // 
+    // If you want Entity Framework to drop and regenerate your database
+    // automatically whenever you change your model schema, please use data migrations.
+    // For more information refer to the documentation:
+    // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+    public Le6pergramDatabase() : base("name=Le6pergramDatabase")
+    {
+    }
+
+    public System.Data.Entity.DbSet<Le6pergram.Web.User> Users { get; set; }
+}
---
+++ Le6pergram.Web/Le6pergram.Web/User.cs
@@ -0,0 +1,38 @@
+﻿namespace Le6pergram.Web
+{    
+    using System.Collections.Generic;
+
+    public class User
+    {
+        public User()
+        {
+            //this.picturesCount = Pictures.Count;
+            this.Followers = new HashSet<User>();
+            this.Following = new HashSet<User>();
+        }
+
+        private int picturesCount;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string RepeatPassword { get; set; }
+
+        public string Biography { get; set; }
+
+        public virtual ICollection<User> Followers { get; set; }
+
+        public virtual ICollection<User> Following { get; set; }
+
+        //ProfilePicture;
+
+        //Pictures;
+    }
+}
